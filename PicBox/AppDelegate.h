fc633d31e6ff8928212257b119e60b3d810a9947
Muhammad Nabeel Arif <nabeel.arif100@gmail.com>
2016-04-14 21:38:18 +0500

    Initial Commit

---
+++ PicBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PicBox
+//
+//  Created by Nabeel Arif on 4/14/16.
+//  Copyright © 2016 Nabeel Arif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PicBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PicBox
+//
+//  Created by Nabeel Arif on 4/14/16.
+//  Copyright © 2016 Nabeel Arif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
